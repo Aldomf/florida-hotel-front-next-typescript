02bc63d0dbe0351030b5cd0439f548fdbec92cf1
Aldo Miralles <alditomiralles3@gmail.com>
2024-05-25 17:38:09 -0600

    creating oneRoom page mobil version

---
+++ src/app/room/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainNavbar from '@/components/main-app/MainNavBar'
+import OneRoom from "@/components/main-app/room/OneRoom";
+
+function Room() {
+  return (
+    <div>
+      <MainNavbar />
+      <OneRoom/>
+    </div>
+  );
+}
+
+export default Room;
---
+++ src/components/main-app/room/OneRoom.tsx
@@ -0,0 +1,162 @@
+"use client";
+import React, { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { useGetRoomByIdQuery } from "@/redux/services/roomApi";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Image from "next/image";
+import { FaWifi } from "react-icons/fa";
+import { BiHandicap } from "react-icons/bi";
+import { CgScreen } from "react-icons/cg";
+import { RiSafeLine } from "react-icons/ri";
+import { GiMeal } from "react-icons/gi";
+
+function OneRoom() {
+  const params = useParams<{ id: string }>();
+  const roomId = params.id;
+
+  const {
+    data: initialRoomData,
+    error: fetchError,
+    isLoading: isFetching,
+    refetch,
+  } = useGetRoomByIdQuery(roomId);
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
+  if (!initialRoomData?.imageUrls || initialRoomData.imageUrls.length === 0) {
+    return null;
+  }
+
+  const [firstImage, ...restImages] = initialRoomData.imageUrls;
+
+  if (isFetching) {
+    return <div>Loading...</div>;
+  }
+
+  if (fetchError) {
+    const errorMessage =
+      "status" in fetchError
+        ? `Error: ${fetchError.status} - ${JSON.stringify(fetchError.data)}`
+        : fetchError.message;
+
+    return <div>Error fetching room data: {errorMessage}</div>;
+  }
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 4,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 2,
+    },
+  };
+
+  return (
+    <div>
+      <div>
+        <h1 className="mb-6 mt-6 text-4xl border-b border-[#9D8000] text-[#9D8000] text-center pb-6">
+          {initialRoomData?.roomNumber} {initialRoomData?.roomType}
+        </h1>
+      </div>
+      <div className="my-6 mx-3">
+        <div className="main-image mb-5">
+          <Image
+            src={firstImage}
+            alt="Main Room Image"
+            width={500}
+            height={500}
+            className="w-full h-auto"
+          />
+        </div>
+        {restImages.length > 0 && (
+          <Carousel
+            responsive={responsive}
+            autoPlay={true}
+            autoPlaySpeed={3000}
+            infinite={true}
+            arrows={false}
+            showDots={true}
+            // itemClass="pr-1"
+          >
+            {restImages.map((url, index) => (
+              <div key={index} className="carousel-image">
+                <Image
+                  src={url}
+                  alt={`Room Image ${index + 2}`}
+                  width={500}
+                  height={500}
+                  className="w-36 ss:w-auto h-auto mx-auto"
+                />
+              </div>
+            ))}
+          </Carousel>
+        )}
+      </div>
+      <div className="mx-8 mt-16 text-center text-[#9D8000]">
+        <p className="mb-4 text-2xl border-b border-[#9D8000] pb-2">
+          {initialRoomData?.roomNumber} {initialRoomData?.roomType}
+        </p>
+        <p className="mb-2">{initialRoomData?.description}</p>
+      </div>
+      <div className="flex flex-col mx-8 mt-8 text-[#9D8000] text-lg">
+        <p className="mb-2">
+          <span className="text-xl font-bold">Price Per Night:</span> $
+          {initialRoomData?.pricePerNight}
+        </p>
+        <p className="mb-2">
+          <span className="text-xl font-bold">Capacity:</span>{" "}
+          {initialRoomData?.capacity} persons
+        </p>
+        <p className="mb-2">
+          <span className="text-xl font-bold">Room Size:</span>{" "}
+          {initialRoomData?.roomSize} sqm
+        </p>
+        <p className="mb-2">
+          {" "}
+          <span className="text-xl font-bold">Availability:</span>
+          {" "}
+          <span className="bg-[#9D8000] text-white px-2 rounded-xl">{initialRoomData?.availabilityStatus}</span>
+        </p>
+      </div>
+      <div className="flex flex-col mx-8 mt-8 text-[#9D8000]">
+        <div className="flex items-center mb-2 border-b border-[#9D8000] pb-2">
+          <FaWifi className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10 mr-6" /> WiFi
+        </div>
+        <div className="flex items-center mb-2 border-b border-[#9D8000] pb-2">
+          <BiHandicap className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10 mr-6" />{" "}
+          Inclusive
+        </div>
+        <div className="flex items-center mb-2 border-b border-[#9D8000] pb-2">
+          <CgScreen className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10 mr-6" />
+          TV
+        </div>
+        <div className="flex items-center mb-2 border-b border-[#9D8000] pb-2">
+          <RiSafeLine className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10 mr-6" />{" "}
+          Safe Box
+        </div>
+        <div className="flex items-center mb-2 border-b border-[#9D8000] pb-2">
+          <GiMeal className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10 mr-6" /> Room
+          Service
+        </div>
+      </div>
+      <div className="flex justify-center mx-8 mt-2 text-[#9D8000]">
+        <button className="bg-[#9D8000] hover:bg-[#C2B266] text-white px-2 py-2 mb-4">Book Now</button>
+      </div>
+    </div>
+  );
+}
+
+export default OneRoom;
