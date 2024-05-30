62dd0d0d364e74aae7faa55ed5fe54b531bafe8f
Aldo Miralles <alditomiralles3@gmail.com>
2024-05-29 22:07:58 -0600

    updating mt again

--- src/components/main-app/rooms/ManyRooms.tsx
+++ src/components/main-app/rooms/ManyRooms.tsx
@@ -34,7 +34,7 @@ const ManyRooms: React.FC<RoomCardProps> = ({ rooms, isLoading, error }) => {
   }
 
   return (
-    <div className="mt-[400px] ss:mt-[480px] lg:mt-[450px]">
+    <div className="mt-[380px] ss:mt-[480px] lg:mt-[450px]">
       {rooms?.map((room, roomIndex) => (
         <div
           key={room.id}
